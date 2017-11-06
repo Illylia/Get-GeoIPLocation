57045e7cb7eae4473e60e96b1db67aa45442f7e2
Illylia <kevin.wilson@illylia.com>
2017-11-06 14:11:13 -0500

    Create PS Script

---
+++ Get-GeoIPLocation.ps1
@@ -0,0 +1,19 @@
+﻿Param
+(
+    [Parameter(Mandatory)]
+    [string]$IPAddress
+)
+ 
+$geoIPRequest = Invoke-RestMethod -Method Get -Uri "http://geoip.nekudo.com/api/$IPAddress"
+    
+$IPObject = New-Object System.Object
+
+$IPObject | Add-Member -Type NoteProperty -Name "IP" -Value $geoIPRequest.IP
+$IPObject | Add-Member -Type NoteProperty -Name "City" -Value $geoIPRequest.City
+$IPObject | Add-Member -Type NoteProperty -Name "Country" -Value $geoIPRequest.Country.Name
+$IPObject | Add-Member -Type NoteProperty -Name "Code" -Value $geoIPRequest.Country.Code
+$IPObject | Add-Member -Type NoteProperty -Name "Latitude" -Value $geoIPRequest.Location.Latitude
+$IPObject | Add-Member -Type NoteProperty -Name "Longitude" -Value $geoIPRequest.Location.Longitude
+$IPObject | Add-Member -Type NoteProperty -Name "TimeZone" -Value $geoIPRequest.Location.Time_zone
+
+return $IPObject
